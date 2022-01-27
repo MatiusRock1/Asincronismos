ad7334e2b08126b53e200f404212078f8caf9249
Jorge Trujillo <88351609+MatiusRock1@users.noreply.github.com>
2022-01-27 09:54:33 -0500

    anidado de promesas

--- src/promise/index.js
+++ src/promise/index.js
@@ -12,3 +12,27 @@ const somethingWillHapped = () => {
 somethingWillHapped()
 .then(Response => console.log(Response))
 .catch(err => console.log(err));
+
+const somethingWillHapped2 = () => {
+    return new Promise((resolve, reject) => {
+        if(true){
+            setTimeout(() => {
+                resolve('True');
+            }, 2000)
+        } else {
+            const error = new Error('Whooop');
+            reject (error);
+        }
+    });
+}
+somethingWillHapped2()
+.then(Response => console.log(Response))
+.catch(err => console.log(err));
+
+Promise.all([somethingWillHapped(), somethingWillHapped2()])
+.then(Response => {
+    console.log("Array of results", Response)
+})
+.catch(err => {
+    console.log(err)
+});
